045777e3a148c58ae63fcae30fb4272970a012de
Ricky Blankenaufulland <zoolway@gmail.com>
2018-05-11 23:20:52 +0200

    new: template for AD settings

---
+++ AureliaBase.Web/src/settings.ts
@@ -0,0 +1,8 @@
+﻿export let apiConfig = {
+    service: 'http://localhost:52676/api/',
+}
+
+export let azureAdConfig = {
+    clientId: 'guid',
+    authority: 'https://login.microsoftonline.com/----',
+}
