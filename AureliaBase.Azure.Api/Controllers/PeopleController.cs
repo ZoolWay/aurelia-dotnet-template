7e5b82279fa65d934ce90dd9e1dd68723f7cfa27
Ricky Blankenaufulland <zoolway@gmail.com>
2018-05-11 21:57:32 +0200

    new: added an Azure-AD protected WebApi

---
+++ AureliaBase.Azure.Api/Controllers/PeopleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AureliaBase.Azure.Api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class PeopleController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(SampleData.PeopleList);
+        }
+    }
+}
---
+++ AureliaBase.Azure.Api/SampleData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AureliaBase.Azure.Api
+{
+    public static class SampleData
+    {
+        public static List<object> PeopleList = new List<object>()
+        {
+            new
+            {
+                Name = "Master Yoda",
+                Email = "master@yoda.com",
+                Id = 1
+            },
+            new
+            {
+                Name = "Luke Skywalker",
+                Email = "luke@skywalker.com",
+                Id = 2
+            },
+            new
+            {
+                Name = "Darth Vader",
+                Email = "darth@vader.com",
+                Id = 3
+            },
+            new
+            {
+                Name = "Leia Organa",
+                Email = "leia@organa.com",
+                Id = 4
+            },
+        };
+    }
+}
